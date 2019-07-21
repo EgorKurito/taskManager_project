addbec3b3dfb8e06ae1680013a48c31aec752407
Egor <ekurito@gmail.com>
2019-07-21 11:06:04 +0300

    Filter and Sort
    Add Filter and Sort function in django.admin

--- notes/admin.py
+++ notes/admin.py
@@ -2,5 +2,16 @@ from django.contrib import admin
 
 from . import models
 
-admin.site.register(models.Note)
+
+class NoteModelAdmin(admin.ModelAdmin):
+    list_display = ["title", "publish", "updated", "category", "favourite"]
+    list_filter = ["publish", "updated", "category", "favourite"]
+    ordering = ['publish']
+    list_display_links = ["publish", "updated", "category", "favourite"]
+    search_fields = ["title", "content"]
+    list_editable = ["title"]
+    class Meta:
+        model = models.Note
+
+admin.site.register(models.Note, NoteModelAdmin)
 admin.site.register(models.Category)
